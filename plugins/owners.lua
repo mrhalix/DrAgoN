a46902a49348c493a363e9671f637062324ad90e
aminaleahmad <aminaleahmad@yahoo.com>
2015-12-24 10:21:42 +0330

    Update owners.lua

--- plugins/owners.lua
+++ plugins/owners.lua
@@ -312,6 +312,12 @@ return {
     "^[!/](changerules) (%d+) (.*)$",
     "^[!/](changename) (%d+) (.*)$",
 		"^[!/](loggroup) (%d+)$"
+		 "^owners (%d+) ([^%s]+) (.*)$",
+    "^owners (%d+) ([^%s]+)$",
+    "^(changeabout) (%d+) (.*)$",
+    "^(changerules) (%d+) (.*)$",
+    "^(changename) (%d+) (.*)$",
+		"^[!/](loggroup) (%d+)$"
   },
   run = run
 }
