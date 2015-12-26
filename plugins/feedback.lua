a28947dcc1ced25a81eb44e8cdf1960a38dfd637
aminaleahmad <aminaleahmad@yahoo.com>
2015-12-26 18:30:40 +0330

    Update feedback.lua

--- plugins/feedback.lua
+++ plugins/feedback.lua
@@ -2,7 +2,7 @@ do
 
 function run(msg, matches)
 
-local fuse = '#newfeedback \n\nid : ' .. msg.from.id .. '\n\nName : ' .. msg.from.print_name ..'\n\nLetters :\n\n\n' .. matches[1] 
+local fuse = '#newfeedback \n\nid : ' .. msg.from.id .. '\n\nUsername: '..v.username..'\n\nName : ' .. msg.from.print_name ..'\n\nLetters :\n\n\n' .. matches[1] 
 local fuses = '!printf user#id' .. msg.from.id
 
 
