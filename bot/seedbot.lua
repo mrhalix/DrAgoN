0146bd704aadbef82501fef68fc2335049a0ff19
aminaleahmad <aminaleahmad@yahoo.com>
2016-01-03 14:46:53 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -72,6 +72,12 @@ function msg_valid(msg)
     print('\27[36mNot valid: Msg from our id\27[39m')
     return false
   end
+  
+  if msg.out then
+    print('\27[36mNot valid: msg from us\27[39m')
+    return false
+  end
+
 
   if msg.to.type == 'encr_chat' then
     print('\27[36mNot valid: Encrypted chat\27[39m')
