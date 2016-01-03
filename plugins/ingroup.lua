920af08ce428256309f8de6433cd5390478dda17
aminaleahmad <aminaleahmad@yahoo.com>
2016-01-03 12:54:58 +0330

    Update ingroup.lua

--- plugins/ingroup.lua
+++ plugins/ingroup.lua
@@ -715,14 +715,14 @@ local function run(msg, matches)
     end
     if matches[1] == 'newlink' then
       if not is_momod(msg) then
-        return "For moderators only!"
+        return "شما دسترسی ندارید"
       end
       local function callback (extra , success, result)
         local receiver = 'chat#'..msg.to.id
         if success == 0 then
            return send_large_msg(receiver, '*ربات ادمین گروه نیست به همین دلیل نمیتواند لینک را ارسال کند*')
         end
-        send_large_msg(receiver, "Created a new link")
+        send_large_msg(receiver, "لینک جدید با موفقیت ساخته شد")
         data[tostring(msg.to.id)]['settings']['set_link'] = result
         save_data(_config.moderation.data, data)
       end
@@ -732,22 +732,22 @@ local function run(msg, matches)
     end
     if matches[1] == 'link' then
       if not is_momod(msg) then
-        return "For moderators only!"
+        return "شما دسترسی ندارید"
       end
       local group_link = data[tostring(msg.to.id)]['settings']['set_link']
       if not group_link then 
-        return "Create a link using /newlink first !"
+        return "اول دستور\n/newlink را بزنید"
       end
        savelog(msg.to.id, name_log.." ["..msg.from.id.."] requested group link ["..group_link.."]")
       return "Group link:\n"..group_link
     end
 	if matches[1] == 'linkpv' then
       if not is_momod(msg) then
-        return "For moderators only!"
+        return "شما دسترسی ندارید"
       end
 if matches[1] == 'pv' then
-      if not is_momod(msg) then
-        return "For moderators only!"
+      if not is_sudo(msg) then
+        return "شما دسترسی ندارید"
       end
 local msgpv = matches[3]
 local rever = matches[2] send_large_msg('user#id'..rever,msgpv.."\n" ,ok_cb , false)
@@ -755,14 +755,14 @@ return "پیام ارسال شد "
     end
       local group_link = data[tostring(msg.to.id)]['settings']['set_link']
       if not group_link then 
-        return "Create a link using /newlink first !"
+        return "اول دستور\n/newlink را بزنید"
       end
        savelog(msg.to.id, name_log.." ["..msg.from.id.."] requested group link ["..group_link.."]")
      send_large_msg('user#id'..msg.from.id, "Group link:\n"..group_link)
     end
     if matches[1] == 'setowner' then
       if not is_owner(msg) then
-        return "For owner only!"
+        return "فقط برای سازنده"
       end
       data[tostring(msg.to.id)]['set_owner'] = matches[2]
       save_data(_config.moderation.data, data)
